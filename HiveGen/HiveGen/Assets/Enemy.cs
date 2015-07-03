bc59d2cf6bf2efcab5e1cdb1ca0eda655420e5b7
dibarbet <dibarbet@gmail.com>
2015-07-03 13:55:46 -0400

    Added notes, began implementation of agents.
    Mover is the interface that controls moving objects.  EnemyAgent is a
    subtype of Mover.  NewBehavior is a script I haven't done yet.

---
+++ HiveGen/HiveGen/Assets/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour, Mover
+{
+    public float Speed { get; set; }
+
+    public Vector3 StartPos { get; set; }
+
+    public Vector3 GoalPos {get; set; }
+
+    public Vector3 Position { get; set; }
+
+    public bool IsMoving {get; set;}
+
+    private GameObject m_EnemyObject;
+    public GameObject EnemyObject
+    {
+        get { return m_EnemyObject; }
+        set
+        {
+            if (value != null)
+            {
+                m_EnemyObject = value;
+            }
+        }
+    }
+
+    float m_MoveStartTime;
+
+    public Enemy(GameObject obj)
+    {
+        EnemyObject = obj;
+        Position = obj.transform.position;
+        IsMoving = false;
+        Speed = .5f;
+    }
+
+    public void MoveTo(Vector3 goal)
+    {
+        GoalPos = goal;
+        StartPos = Position;
+        IsMoving = true;
+        m_MoveStartTime = Time.time;
+    }
+
+    public void StopMoving()
+    {
+        IsMoving = false;
+    }
+
+    void Update()
+    {
+        if (IsMoving)
+        {
+            EnemyObject.transform.position = Vector3.Lerp(StartPos, GoalPos, Speed * (Time.time - m_MoveStartTime));
+        }
+    }
+
+    void Start()
+    {
+
+    }
+}
---
+++ HiveGen/HiveGen/Assets/Mover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Mover
+{
+    float Speed
+    {
+        get;
+        set;
+    }
+
+    //Common methods of getting position return 3d vector
+    Vector3 StartPos
+    {
+        get;
+        set;
+    }
+
+    Vector3 GoalPos
+    {
+        get;
+        set;
+    }
+
+    Vector3 Position
+    {
+        get;
+        set;
+    }
+
+    bool IsMoving
+    {
+        get;
+        set;
+    }
+
+    void StopMoving();
+
+    void MoveTo(Vector3 goal);
+
+}
