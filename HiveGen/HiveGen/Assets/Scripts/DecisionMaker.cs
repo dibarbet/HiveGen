7579fa4ed239bf9875c969617b1e082ebc83aa56
dibarbet <dibarbet@gmail.com>
2015-07-12 19:48:42 -0400

    Initial setup for decision making
    Created class and node struct

---
+++ HiveGen/HiveGen/Assets/Scripts/DecisionMaker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecisionMaker : MonoBehaviour {
+
+    public struct Node<T>
+    {
+        public string name { get; set; }
+        public T value { get; set; }
+        public Node(string name, T value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Node<bool> attack = new Node<bool>("ATTACK", false);
+        Node<bool> defend = new Node<bool>("DEFEND", false);
+        Node<bool> dodge = new Node<bool>("DODGE", false);
+        Node<bool> roam = new Node<bool>("ROAM", false);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
